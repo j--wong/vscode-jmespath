63d82e2189e15079d355cb605d9663dbd306ae5f
Josh Wong <josh.wong@versent.com.au>
2017-01-02 23:25:47 +1100

    Ensure only one output channel is created on extension activation

--- src/jmespathMain.ts
+++ src/jmespathMain.ts
@@ -4,10 +4,11 @@ import * as vscode from "vscode";
 import { queryJson } from "./jmespathQuery";
 
 export function activate(context: vscode.ExtensionContext) {
+	let outputChannel = vscode.window.createOutputChannel("JMESPath Output");
+
 	context.subscriptions.push(
 		vscode.commands.registerTextEditorCommand("jmespath.query",
 			(textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
-				let outputChannel = vscode.window.createOutputChannel("JMESPath Output");
 				queryJson(context, outputChannel);
 			}
 		)
